47cbcd36fbe1d94c99b4cac5664ab30ad6293ba7
Freedy <21914103@zju.edu.cn>
2020-08-19 12:28:13 +0800

    deploy

---
+++ precache-manifest.8fc2612007be0663a602b530c6a189c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "596d7b16b7880e5408b3db9a29806b41",
+    "url": "/charge-account-webst/index.html"
+  },
+  {
+    "revision": "095f8f49f7d5a7380543",
+    "url": "/charge-account-webst/static/css/main.7c64c589.chunk.css"
+  },
+  {
+    "revision": "4dce15c99492ac51eb85",
+    "url": "/charge-account-webst/static/js/2.b6ef5d40.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/charge-account-webst/static/js/2.b6ef5d40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "095f8f49f7d5a7380543",
+    "url": "/charge-account-webst/static/js/main.150be0f2.chunk.js"
+  },
+  {
+    "revision": "f4d158e2dbb35eafb646",
+    "url": "/charge-account-webst/static/js/runtime-main.3264fdc8.js"
+  }
+]);
